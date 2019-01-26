99cb5dce35c09e4190a653cdd1ba83f662d685db
bdhamme <brianhammer1122@gmail.com>
2019-01-26 15:30:20 -0600

    Getting Started

---
+++ src/bmiandstore/BMIandStore.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bmiandstore;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bham2
+ */
+public class BMIandStore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean RUN=true;
+        while(RUN==true)
+        {
+    Scanner reader= new Scanner(System.in);//Reading from the System in 
+        System.out.println("What is the person height in centimeters");
+    double height=reader.nextInt();
+    System.out.println("what is the person weight in kg(1 kg=2.2kg)?");
+    double weight=reader.nextInt();
+    System.out.println("BMI="+weight/height);
+    System.out.println("Whould you like to run Agian? True or False");
+    String RUN2=reader.toString();
+    if (RUN2.equals("True") || RUN2.equals("true"))
+    {
+        RUN=true;
+    }
+    else if (RUN2.equals("False") || RUN2.equals("false"))
+    {
+        RUN=false;
+    }
+        }
+        // TODO code application logic here
+    }
+    
+}
